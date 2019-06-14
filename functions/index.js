7a8434f8d3bcd7e5a055e405ca00e2e79a69522a
Jesús <gjmg1612@gmail.com>
2019-06-14 11:37:22 -0500

    routes created

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const uuid = require('uuid/v4')
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const  db = admin.firestore();
+const id = uuid();
+
+exports.addUser = functions.https.onRequest((req, res) => {
+ const { name, address, age , type } = req.query;
+
+ const docRef = db.collection('users').doc(id);
+ docRef.set({name, address, age , type });
+ 
+ res.send('usuario creado con id: '+ id);
+});
+
+exports.getUser = functions.https.onRequest( async (req, res) => {
+  const user = await db.collection('users').doc(req.query.id).get();
+
+  res.json({info: user.data(), id: user.id });
+})
